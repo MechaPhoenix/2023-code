c83a10053aa5c08a581164ecf155a292538c61fc
MechaPhoenix <rez.mecha7722@gmail.com>
2023-03-16 15:47:40 -0400

    added auto-balance code

---
+++ src/main/include/autoBalance.h
@@ -0,0 +1,30 @@
+// This code was taken from team DAVE's github page:
+// https://github.com/FRC3683/OpenAutoBalance
+
+#pragma once
+#include <frc/BuiltInAccelerometer.h>
+#include <cmath>
+
+class autoBalance{
+    public:
+        autoBalance();
+        double getPitch();
+        double getRoll();
+        double getTilt();
+        double autoBalanceRoutine();
+        double scoreAndBalance();
+        int secondsToTicks(double time);
+        
+    private:
+        frc::BuiltInAccelerometer mAccel{};
+        int state;
+        int debounceCount;
+        double robotSpeedSlow;
+        double robotSpeedFast;
+        double onChargeStationDegree;
+        double levelDegree;
+        double debounceTime;
+        double singleTapTime;
+        double scoringBackUpTime;
+        double doubleTapTime;
+};
